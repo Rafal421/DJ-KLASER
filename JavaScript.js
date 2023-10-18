21dc9aa3319c570aaa9cd14285072831a70028b6
Rafal421 <rhaczek421@gmail.com>
2023-10-18 18:17:00 +0200

    maybe new better dj-klaser web

--- JavaScript.js
+++ JavaScript.js
@@ -1,6 +1,8 @@
 const menuToggle = document.querySelector('.toggle');
 const showcase = document.querySelector('.showcase');
 
+const glitch  = '/▒/ ▒██/▓ <▒█░▓ ░▒< ▓▒<▓< ▒██▒ ▓█▓ ███/ ▓▓░▒';
+
 menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('active');
   showcase.classList.toggle('active');
@@ -17,8 +19,47 @@ setTimeout(()=>{
 
 const text = baffle(".Name");
 text.set({
-characters : '/▒/ ▒██/▓ <▒█░▓ ░▒< ▓▒<▓< ▒██▒ ▓█▓ ███/ ▓▓░▒',
+characters : glitch,
 speed:150
 });
 text.start();
 text.reveal(4000);
+
+
+const textH2 = baffle("#glitchH2");
+const textH3 = baffle("#glitchH3");
+const textP = baffle("#glitchP");
+const textLink = baffle("#glitchLink");
+
+textH2.set({
+  characters: glitch,
+  speed: 150,
+});
+textH3.set({
+  characters: glitch,
+  speed: 150,
+});
+textP.set({
+  characters: glitch,
+  speed: 150,
+});
+textLink.set({
+  characters: glitch,
+  speed: 200,
+});
+
+
+
+textH2.start();
+textH3.start();
+textP.start();
+
+textH2.reveal(5000);
+textH3.reveal(3000);
+textP.reveal(2200);
+textLink.reveal(8000);
+
+
+
+
+
